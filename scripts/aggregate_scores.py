a044bfda210a4e3e5fe1392957fca5ca1c3bfd3f
Richard Barnes <rbarnes@umn.edu>
2018-02-01 13:00:37 -0800

    Modified to include population data

--- scripts/aggregate_scores.py
+++ scripts/aggregate_scores.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import csv
-import glob
-import sys
-import pandas as pd
 import fiona
 import functools
+import glob
+import numpy as np
 import os
-import string
+import pandas as pd
 import pickle
+import string
+import sys
 
 fips = {'01':'AL','02':'AK','04':'AZ','05':'AR','06':'CA','08':'CO','09':'CT','10':'DE','11':'DC','12':'FL','13':'GA','15':'HI','16':'ID','17':'IL','18':'IN','19':'IA','20':'KS','21':'KY','22':'LA','23':'ME','24':'MD','25':'MA','26':'MI','27':'MN','28':'MS','29':'MO','30':'MT','31':'NE','32':'NV','33':'NH','34':'NJ','35':'NM','36':'NY','37':'NC','38':'ND','39':'OH','40':'OK','41':'OR','42':'PA','44':'RI','45':'SC','46':'SD','47':'TN','48':'TX','49':'UT','50':'VT','51':'VA','53':'WA','54':'WV','55':'WI','56':'WY','60':'AS','66':'GU','69':'MP','72':'PR','74':'UM','78':'VI'}
 
@@ -43,85 +44,105 @@ def ProcScores(geotype, sups, fileglob):
   return df
 
 
-if len(sys.argv)!=6:
-  print("Syntax: {0} <Parents File> <Tract Scores> <Block Group Scores> <Block Scores> <Output File>".format(sys.argv[0]))
+if len(sys.argv)!=7:
+  print("Syntax: {0} <Parents File> <Tract Scores> <Block Group Scores> <Block Scores> <Population Data> <Output File>".format(sys.argv[0]))
   sys.exit(-1)
 
-output_file = sys.argv[5]
+superunits_file = os.path.expandvars(sys.argv[1])
+tract_glob      = os.path.expandvars(sys.argv[2])
+blockgroup_glob = os.path.expandvars(sys.argv[3])
+block_glob      = os.path.expandvars(sys.argv[4])
+population_data = os.path.expandvars(sys.argv[5])
+output_file     = os.path.expandvars(sys.argv[6])
 
 print("Loading superunits...")
-sup_filename = sys.argv[1]
+sup_filename = superunits_file
 sup_fiona    = fiona.open(sup_filename)
 sup_prj      = sup_fiona.crs
 sups         = [x for x in sup_fiona]
 
+if not os.path.exists("aggregate-score-geographic-data.pickle"):
+  print("Loading tract data...")
+  dftract = ProcScores('tract',      sups, tract_glob)
 
-dftract = ProcScores('tract',      sups, sys.argv[2])
+  if blockgroup_glob!='-':
+    print("Loading block group data...")
+    dfbg    = ProcScores('blockgroup', sups, blockgroup_glob)
+  else:
+    dfbg = pd.DataFrame()
 
-if sys.argv[3]!='-':
-  dfbg    = ProcScores('blockgroup', sups, sys.argv[3])
+  if block_glob!='-':
+    print("Loading block data...")
+    dfblock = ProcScores('block',      sups, block_glob)
+  else:
+    dfblock = pd.DataFrame()
+
+  df = pd.concat([dftract, dfbg, dfblock], ignore_index=True)
+
+  df = df.rename(columns={'external':'Int_or_Ext'})
+  df['Int_or_Ext'] = df['Int_or_Ext'].replace(True,  'Boundary')
+  df['Int_or_Ext'] = df['Int_or_Ext'].replace(False, 'Interior')
+
+  pickle.dump(df, open( "aggregate-score-geographic-data.pickle", "wb" ) )
 else:
-  dfbg = pd.DataFrame()
+  print("Loading pickled data 'aggregate-score-geographic-data.pickle'...")
+  df = pickle.load(open("aggregate-score-geographic-data.pickle", "rb"))
 
-if sys.argv[4]!='-':
-  dfblock = ProcScores('block',      sups, sys.argv[4])
-else:
-  dfblock = pd.DataFrame()
+raise Exception("test")
 
-df = pd.concat([dftract, dfbg, dfblock], ignore_index=True)
+#Load population data
+popdata = pd.read_csv(population_data, sep=' ', low_memory=False, header=0, dtype={'BLOCKID10': str, 'POP10': np.int32})
+popdata = popdata.rename(columns={'BLOCKID10': 'blockid', 'POP10': 'population'})
 
-pickle.dump(df, open( "aggregate-score-intermediate-dataframe.pickle", "wb" ) )
+popdata['tractid']  = popdata['blockid'].apply(lambda x: x[0:11])
+popdata['blkgrpid'] = popdata['blockid'].apply(lambda x: x[0:12])
+
+blockpop  = popdata[['blockid', 'population']]
+blkgrppop = popdata.groupby('blkgrpid').sum().reset_index()
+tractpop  = popdata.groupby('tractid').sum().reset_index()
+
+blockpop  = blockpop.rename (columns={'blockid':  'geoid'})
+blkgrppop = blkgrppop.rename(columns={'blkgrpid': 'geoid'})
+tractpop  = tractpop.rename (columns={'tractid':  'geoid'})
+
+popdata = pd.concat([blockpop,blkgrppop,tractpop], ignore_index=True)
+
+merged = pd.merge(df, popdata, left_on='child', right_on='geoid', how='left')
+del merged['geoid']
+
+merged['WeightedPop'] = merged['per']*merged['population']
+merged['WeightedPop'] = merged['WeightedPop'].fillna(value=0)
+
+df = merged
+
+
+
 
 df50         = df.loc[df['per']>0.5]
-df50         = df50.groupby(["parent","geotype","external"]).agg({'child':'count'}).reset_index()
-df50         = df50.pivot_table(index=['parent','geotype'], columns='external')
-df50.columns = df50.columns.droplevel().rename(None)
+df50         = df50.groupby(["parent","geotype","Int_or_Ext"]).agg({'child':'count', 'WeightedPop':'sum'}).reset_index()
+df50         = df50.rename(columns={'child': 'Count'})
+df50         = df50.pivot_table(index=['parent','geotype'], columns='Int_or_Ext')
+df50.columns = ['_'.join((col[1], col[0])) for col in df50.columns]
 df50         = df50.reset_index().fillna("null")
-df50         = df50.rename(columns={True: "Boundary50", False: "Interior50"})
+df50         = df50.pivot_table(index=['parent'], columns='geotype')
+df50.columns = ['_'.join((col[1].title(), col[0]))+"_50" for col in df50.columns]
+df50         = df50.reset_index().fillna("null")
 
 df10         = df.loc[df['per']>0.1]
-df10         = df10.groupby(["parent","geotype","external"]).agg({'child':'count'}).reset_index()
-df10         = df10.pivot_table(index=['parent','geotype'], columns='external')
-df10.columns = df10.columns.droplevel().rename(None)
+df10         = df10.groupby(["parent","geotype","Int_or_Ext"]).agg({'child':'count', 'WeightedPop':'sum'}).reset_index()
+df10         = df10.rename(columns={'child': 'Count'})
+df10         = df10.pivot_table(index=['parent','geotype'], columns='Int_or_Ext')
+df10.columns = ['_'.join((col[1], col[0])) for col in df10.columns]
 df10         = df10.reset_index().fillna("null")
-df10         = df10.rename(columns={True: "Boundary10", False: "Interior10"})
+df10         = df10.pivot_table(index=['parent'], columns='geotype')
+df10.columns = ['_'.join((col[1].title(), col[0]))+"_10" for col in df10.columns]
+df10         = df10.reset_index().fillna("null")
 
-#Merge into df10 for output
-df10['Interior50'] = df50['Interior50']
-df10['Boundary50'] = df50['Boundary50']
 
-df10 = df10.rename(columns={'parent':'DistrictID'})
+dfout = pd.merge(df10,df50,on="parent")
+dfout = dfout.rename(columns={'parent':'DistrictID'})
 
-dfint10 = df10[['DistrictID','geotype','Interior10']]
-dfint10 = dfint10.pivot(index='DistrictID', columns='geotype', values='Interior10')
-dfint10 = dfint10.rename(columns={'tract':'TractInterior10', 'block':'BlockInterior10', 'blockgroup':'BGInterior10'})
-dfint10.index.name = 'DistrictID'
-dfint10.reset_index(inplace=True)
-
-dfext10 = df10[['DistrictID','geotype','Boundary10']]
-dfext10 = dfext10.pivot(index='DistrictID', columns='geotype', values='Boundary10')
-dfext10 = dfext10.rename(columns={'tract':'TractBoundary10', 'block':'BlockBoundary10', 'blockgroup':'BGBoundary10'})
-dfext10.index.name = 'DistrictID'
-dfext10.reset_index(inplace=True)
-
-dfint50 = df10[['DistrictID','geotype','Interior50']]
-dfint50 = dfint50.pivot(index='DistrictID', columns='geotype', values='Interior50')
-dfint50 = dfint50.rename(columns={'tract':'TractInterior50', 'block':'BlockInterior50', 'blockgroup':'BGInterior50'})
-dfint50.index.name = 'DistrictID'
-dfint50.reset_index(inplace=True)
-
-dfext50 = df10[['DistrictID','geotype','Boundary50']]
-dfext50 = dfext50.pivot(index='DistrictID', columns='geotype', values='Boundary50')
-dfext50 = dfext50.rename(columns={'tract':'TractBoundary50', 'block':'BlockBoundary50', 'blockgroup':'BGBoundary50'})
-dfext50.index.name = 'DistrictID'
-dfext50.reset_index(inplace=True)
-
-df_final = functools.reduce(lambda left,right: pd.merge(left,right,on='DistrictID'), [dfint10,dfext10,dfint50,dfext50])
-
-df_final['DistrictName'] = df_final['DistrictID'].apply(lambda x: fips[x[0:2]] + ' District ' + x[2:])
+dfout['DistrictName'] = dfout['DistrictID'].apply(lambda x: fips[x[0:2]] + ' District ' + x[2:])
 
 #Reorder columns
-columns = ['DistrictID', 'DistrictName', 'TractInterior10','TractBoundary10','TractInterior50','TractBoundary50','BGInterior10','BGBoundary10','BGInterior50','BGBoundary50','BlockInterior10','BlockBoundary10','BlockInterior50','BlockBoundary50']
-df_final = df_final[columns]
-
-df_final.to_csv(output_file, sep=",", index=None, columns=columns, quoting=csv.QUOTE_ALL)
+dfout.to_csv(output_file, sep=",", index=None, quoting=csv.QUOTE_ALL)
